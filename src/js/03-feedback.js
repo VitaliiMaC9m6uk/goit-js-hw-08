87ecbcd3ac3e54a63f61c84694aae570ba09bba4
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-03-09 16:22:54 +0100

    зробив 2 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+
+
+form.addEventListener('input', saveFeedbackInLocal);
+
+function saveFeedbackInLocal(event) {
+    const {
+        elements: { email, message }
+    } = event.currentTarget;
+    localStorage.setItem('feedback-form-state',
+        JSON.stringify({ email: email.value, message: message.value }));
+}
+
+const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (savedFeedback) {
+    const inputValue = savedFeedback.email;
+    const textAreaValue = savedFeedback.message;
+    input.value = inputValue; 
+    textArea.value = textAreaValue;
+}
 
