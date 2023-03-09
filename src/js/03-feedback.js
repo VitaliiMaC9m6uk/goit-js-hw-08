4cccba6272638fa5954926897b3ab9db42d99f41
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-03-09 19:40:52 +0100

    створив ключ
    STORAGE_KEY

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textArea = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(saveFeedbackInLocal, 500));
 form.addEventListener('submit', onSubmitForm);
@@ -11,18 +12,18 @@ function onSubmitForm(event) {
     event.preventDefault();
     console.log('Відправляємо форму');
     event.currentTarget.reset();
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     localStorage.clear();
 }
 function saveFeedbackInLocal(event) {
     const {
         elements: { email, message }
     } = event.currentTarget;
-    localStorage.setItem('feedback-form-state',
+    localStorage.setItem(STORAGE_KEY,
         JSON.stringify({ email: email.value, message: message.value }));
 }
 
-const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+const savedFeedback = JSON.parse(localStorage.getItem(STORAGE_KEY));
 if (savedFeedback) {    
     input.value = savedFeedback.email; 
     textArea.value = savedFeedback.message;
