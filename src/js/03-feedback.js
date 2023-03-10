05322806c8c96e3e2d6ca97df923e94b9fa7bb34
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-03-10 11:45:48 +0100

    зробив 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,6 +4,7 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textArea = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
 
 form.addEventListener('input', throttle(saveFeedbackInLocal, 500));
 form.addEventListener('submit', onSubmitForm);
@@ -13,14 +14,19 @@ function onSubmitForm(event) {
     console.log('Відправляємо форму');
     event.currentTarget.reset();
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-    localStorage.clear();
+    localStorage.removeItem(STORAGE_KEY);
+
 }
 function saveFeedbackInLocal(event) {
-    const {
-        elements: { email, message }
-    } = event.currentTarget;
-    localStorage.setItem(STORAGE_KEY,
-        JSON.stringify({ email: email.value, message: message.value }));
+    if (savedFeedback) {
+        savedFeedback[event.target.name] = event.target.value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(savedFeedback));
+        return;
+    }
+    formData[event.target.name] = event.target.value;
+    console.log(formData)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
 }
 
 const savedFeedback = JSON.parse(localStorage.getItem(STORAGE_KEY));
